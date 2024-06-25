ccc3b8d89e2779d1d465e9181815860b1eca30c7
jhc10031 <jhc10031@nyu.edu>
2024-06-25 03:47:35 -0400

    attempt2

--- main.py
+++ main.py
@@ -5,3 +5,5 @@ from mood_assessor import assess_mood
 
 def main():
     assess_mood()
+
+main()
--- mood_assessor.py
+++ mood_assessor.py
@@ -8,19 +8,23 @@ import os
 def assess_mood():
     moods = ['happy', 'relaxed', 'apathetic', 'sad', 'angry',]
     client_mood = input("Your mood today: ")
+    
+
     while client_mood not in moods:
         client_mood = input("Your mood today:")
-    def mood_to_num():
-        if client_mood == 'happy':
-            return 2
-        elif client_mood == 'relaxed':
-            return 1
-        elif client_mood == 'apathetic':
-            return 0
-        elif client_mood == 'sad':
-            return -1
-        elif client_mood == 'angry':
-            return -2
-    if client_mood in moods:
-        mood_to_num()
-        open("mood_diary.txt", "w")
+    if client_mood == 'happy':
+        return 2
+    elif client_mood == 'relaxed':
+        return 1
+    elif client_mood == 'apathetic':
+        return 0
+    elif client_mood == 'sad':
+        return -1
+    elif client_mood == 'angry':
+        return -2
+    
+    file_open = open("mood_diary.txt", mode="w")
+    file_open.write(client_mood)
+    
+    date_today = datetime.date.today()
+    date_today = str(date_today)
